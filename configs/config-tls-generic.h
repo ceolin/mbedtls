2de11fed321751a4647379df7afd1b4e8f6b7623
Emil Hammarstrom <emil.hammarstrom@assaabloy.com>
2020-10-19 16:49:58 +0200

    configs: enable alpn extensions define via kconfig
    Signed-off-by: Emil Hammarstrom <emil.hammarstrom@assaabloy.com>

--- configs/config-tls-generic.h
+++ configs/config-tls-generic.h
@@ -289,6 +289,10 @@
 #define MBEDTLS_SSL_EXPORT_KEYS
 #endif
 
+#if defined(CONFIG_MBEDTLS_SSL_ALPN)
+#define MBEDTLS_SSL_ALPN
+#endif
+
 #if defined(CONFIG_MBEDTLS_CIPHER)
 #define MBEDTLS_CIPHER_C
 #endif
